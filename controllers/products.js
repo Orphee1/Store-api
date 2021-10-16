68515764fb42bd6ef9cc5026e2238bb571c1499e
Orphee1 <hugolattard@gmail.com>
2021-10-16 17:08:50 +0200

    Added Product Model

--- controllers/products.js
+++ controllers/products.js
@@ -1,9 +1,19 @@
+const Product = require('../models/product')
+
 const getAllProductsStatic = async (req, res) => {
-  throw new Error('Testing async errors package')
-  res.status(200).json({ msg: 'Products testing route' })
+  const products = await Product.find({})
+
+  res.status(200).json({ nbHits: products.length, products })
 }
 const getAllProducts = async (req, res) => {
-  res.status(200).json({ msg: 'Products route' })
+  const { featured } = req.query
+  const queryObject = {}
+  if (featured) {
+    queryObject.featured = featured === 'true' ? 'true' : 'false'
+  }
+  console.log(queryObject)
+  const products = await Product.find(queryObject)
+  res.status(200).json({ nbHits: products.length, products })
 }
 
 module.exports = { getAllProducts, getAllProductsStatic }
